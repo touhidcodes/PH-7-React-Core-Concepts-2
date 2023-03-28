53c4f69e0cdb49819685ffadc0b596bf08a6265d
Touhidur Zaman <touhidcodes@gmail.com>
2023-03-28 21:03:24 +0600

    multiple-data

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,34 @@ import "./App.css";
 function App() {
 	const [count, setCount] = useState(0);
 
-	return <div className='App'>
-<Product></Product>
-  </div>;
+	const products = [
+		{ name: "Laptop", price: 57000 },
+		{ name: "Phone", price: 50000 },
+		{ name: "Watch", price: 7000 },
+		{ name: "Pad", price: 15000 },
+		{ name: "Google Fit", price: 10000 },
+		{ name: "Books", price: 600 },
+		{ name: "Soap", price: 60 },
+	];
+
+	return (
+		<div className='App'>
+			{products.map((product) => (
+				<Product name={product.name} price={product.price}></Product>
+			))}
+			{/* <Product name={"Laptop"} price={2300}></Product>
+			<Product name={"Phone"} price={2000}></Product> */}
+		</div>
+	);
 }
 
-function Product(props){
-  return (
-    <div>
-      <h3>Name: </h3>
-      <p>Price: </p>
-    </div>
-  )
+function Product(props) {
+	return (
+		<div className='product'>
+			<h3>Name: {props.name}</h3>
+			<p>Price: {props.price}</p>
+		</div>
+	);
 }
 
 export default App;
